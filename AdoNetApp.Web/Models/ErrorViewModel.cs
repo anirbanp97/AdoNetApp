483285955e13944c6e8005258cca33b75a549c01
anirbanp97 <anirbanp97@gmail.com>
2025-07-10 19:29:09 +0530

    Initial commit - AdoNetApp multi-layered architecture

---
+++ AdoNetApp.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace AdoNetApp.Web.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
